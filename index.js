96655fe54e533012ace46030937ff61c9420b0fd
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-08-15 13:38:58 -0400

    connected to DB

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.static("public"));
+
+//connect app to mongoDB
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log("connected to DB!");
+  } catch (err) {
+    console.error(err);
+  }
+};
+connectToDB();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
