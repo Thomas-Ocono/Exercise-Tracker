002fb7178fd86613c9e39244d09c61b54d6125b3
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-08-15 15:08:57 -0400

    can create users on db

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 //connect app to mongoDB
 const connectToDB = async () => {
@@ -19,10 +20,39 @@ const connectToDB = async () => {
 };
 connectToDB();
 
+//user schema and model
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+const user = mongoose.model("user", userSchema);
+
+// create new user on db
+const makeNewUser = async (inputName) => {
+  const newUser = new user({
+    name: inputName,
+  });
+  try {
+    newUser.save();
+    console.log("New user created");
+  } catch (err) {
+    console.error(err);
+    console.log("shits fucked");
+  }
+};
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/users", (req, res) => {
+  const inputText = req.body.username;
+  console.log(inputText);
+  makeNewUser(inputText);
+  res.send("Made new user");
+});
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
